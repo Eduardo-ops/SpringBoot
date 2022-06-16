ac902563f2594a037925fa0f7876601d26757b43
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-06-16 13:49:33 -0300

    Anotacoes configuration e bean implementado

--- Vendas/src/main/java/io/project/springboot/Vendas/VendasApplication.java
+++ Vendas/src/main/java/io/project/springboot/Vendas/VendasApplication.java
@@ -1,5 +1,7 @@
 package io.project.springboot.Vendas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
+	@Autowired
+	@Qualifier("applicationName")
+	private String applicationName;
+
 	@GetMapping("/hello")
 	public String helloWord() {
 		return "Hello Word";
+	}
+
+	@GetMapping("/applicationName")
+	public String BeanConfigurationTest() {
+		return applicationName;
 	}
 
 	public static void main(String[] args) {
